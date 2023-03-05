6d83616fc55f09bb98ea5480e45fb6e82b38e8dd
Neil Carlo Sucuangco <succute@yahoo.com>
2023-03-05 11:04:36 +0800

    wip

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,163 @@
+<template>
+  <div class="min-h-full">
+    <header class="bg-white shadow">
+      <div class="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
+        <Popover class="flex h-16 justify-between" v-slot="{ open }">
+          <div class="flex px-2 lg:px-0">
+            <div class="flex flex-shrink-0 items-center">
+              <a href="#">
+                <img class="h-8 w-auto" src="https://img.icons8.com/fluency/256/rainy-weather.png" alt="Your Company" />
+              </a>
+            </div>
+            <nav aria-label="Global" class="hidden lg:ml-6 lg:flex lg:items-center lg:space-x-4">
+              <RouterLink
+                v-for="item in navigation"
+                :key="item.href"
+                :to="item.href"
+                class="px-3 py-2 text-sm font-medium text-gray-900">
+                {{ item.name }}
+              </RouterLink>
+            </nav>
+          </div>
+          <div class="flex flex-1 items-center justify-center px-2 lg:ml-6 lg:justify-end">
+            <div class="w-full max-w-lg lg:max-w-xs">
+              <label for="search" class="sr-only">Search</label>
+              <div class="relative">
+                <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                  <MagnifyingGlassIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                </div>
+                <input id="search" name="search" class="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6" placeholder="Search" type="search" />
+              </div>
+            </div>
+          </div>
+          <div class="flex items-center lg:hidden">
+            <PopoverButton class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
+              <span class="sr-only">Open main menu</span>
+              <Bars3Icon class="block h-6 w-6" aria-hidden="true" />
+            </PopoverButton>
+          </div>
+          <TransitionRoot as="template" :show="open">
+            <div class="lg:hidden">
+              <TransitionChild as="template" enter="duration-150 ease-out" enter-from="opacity-0" enter-to="opacity-100" leave="duration-150 ease-in" leave-from="opacity-100" leave-to="opacity-0">
+                <PopoverOverlay class="fixed inset-0 z-20 bg-black bg-opacity-25" aria-hidden="true" />
+              </TransitionChild>
+
+              <TransitionChild as="template" enter="duration-150 ease-out" enter-from="opacity-0 scale-95" enter-to="opacity-100 scale-100" leave="duration-150 ease-in" leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95">
+                <PopoverPanel focus class="absolute top-0 right-0 z-30 w-full max-w-none origin-top transform p-2 transition">
+                  <div class="divide-y divide-gray-200 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                    <div class="pt-3 pb-2">
+                      <div class="flex items-center justify-between px-4">
+                        <div>
+                          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=600" alt="Your Company" />
+                        </div>
+                        <div class="-mr-2">
+                          <PopoverButton class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
+                            <span class="sr-only">Close menu</span>
+                            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                          </PopoverButton>
+                        </div>
+                      </div>
+                      <div class="mt-3 space-y-1 px-2">
+                        <a v-for="item in navigation" :key="item.name" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800">{{ item.name }}</a>
+                      </div>
+                    </div>
+                    <div class="pt-4 pb-2">
+                      <div class="flex items-center px-5">
+                        <div class="flex-shrink-0">
+                          <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
+                        </div>
+                        <div class="ml-3">
+                          <div class="text-base font-medium text-gray-800">{{ user.name }}</div>
+                          <div class="text-sm font-medium text-gray-500">{{ user.email }}</div>
+                        </div>
+                        <button type="button" class="ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+                          <span class="sr-only">View notifications</span>
+                          <BellIcon class="h-6 w-6" aria-hidden="true" />
+                        </button>
+                      </div>
+                      <div class="mt-3 space-y-1 px-2">
+                        <a v-for="item in userNavigation" :key="item.name" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800">{{ item.name }}</a>
+                      </div>
+                    </div>
+                  </div>
+                </PopoverPanel>
+              </TransitionChild>
+            </div>
+          </TransitionRoot>
+          <div class="hidden lg:ml-4 lg:flex lg:items-center">
+            <button type="button" class="flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+              <span class="sr-only">View notifications</span>
+              <BellIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+
+            <Menu as="div" class="relative ml-4 flex-shrink-0">
+              <div>
+                <MenuButton class="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+                  <span class="sr-only">Open user menu</span>
+                  <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
+                </MenuButton>
+              </div>
+              <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
+                    <a :href="item.href" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</a>
+                  </MenuItem>
+                </MenuItems>
+              </transition>
+            </Menu>
+          </div>
+        </Popover>
+      </div>
+    </header>
+
+    <main class="py-10">
+      <RouterView />
+    </main>
+  </div>
+</template>
+
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-</script>
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Popover,
+  PopoverButton,
+  PopoverOverlay,
+  PopoverPanel,
+  TransitionChild,
+  TransitionRoot,
+} from '@headlessui/vue'
+import {
+  CheckIcon,
+  HandThumbUpIcon,
+  MagnifyingGlassIcon,
+  UserIcon,
+} from '@heroicons/vue/20/solid'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
 
-<template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
-  <RouterView />
-</template>
+const user = {
+  name: 'John Doe',
+  email: 'joendoe@website.com',
+  imageUrl:
+    'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=717&q=80',
+}
+const navigation = [
+  { name: 'Home', href: '/' },
+]
+const breadcrumbs = [
+  { name: 'Jobs', href: '#', current: false },
+  { name: 'Front End Developer', href: '#', current: false },
+  { name: 'Applicants', href: '#', current: true },
+]
+const userNavigation = [
+  { name: 'Your Profile', href: '#' },
+  { name: 'Settings', href: '#' },
+  { name: 'Sign out', href: '#' },
+]
+</script>
 
 <style scoped>
 
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,50 @@
-<script setup lang="ts">
-import ApiTest from "@/components/ApiTest.vue";
-</script>
-
 <template>
-  <main>
-    <api-test></api-test>
-  </main>
+  <div class="mx-auto max-w-7xl pb-12 px-2 sm:px-4">
+    <div class="px-2 sm:px-6">
+      <div class="sm:flex sm:items-center">
+        <div class="sm:flex-auto">
+          <h1 class="text-base font-semibold leading-6 text-gray-900">User Weather</h1>
+          <p class="mt-2 text-sm text-gray-700">A list of all the users weather data.</p>
+        </div>
+      </div>
+      <div class="mt-8 flow-root">
+        <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table class="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">Name</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Email</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Latitude</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Longitude</th>
+                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-3">
+                    <span class="sr-only">View</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="bg-white">
+                <tr v-for="(person, personIdx) in people" :key="person.email" :class="personIdx % 2 === 0 ? undefined : 'bg-gray-50'">
+                  <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">{{ person.name }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.email }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.latitude }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.longitude }}</td>
+                  <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900"
+                      >View<span class="sr-only">, {{ person.name }}</span></a
+                    >
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+const people = [
+  { name: 'Lindsay Walton', email: 'lindsay.walton@example.com', latitude: '123', longitude: '321' },
+]
+</script>
